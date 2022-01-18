2c7b398fbdb37a1109c2eebaf2a559776f06b63e
themisterpig <josedfl2001@gmail.com>
2022-01-18 17:53:30 +0000

    a

--- tester/Program.cs
+++ tester/Program.cs
@@ -1,4 +1,32 @@
-﻿
-int? n = null;
-n=5;
-Console.WriteLine(n);
+﻿int[] flowerbed = { 1, 0, 0, 0, 1};
+ int j = 0;
+        int x = 0;
+        if(flowerbed[0]== 0 && flowerbed[1]== 0){
+            x=x+1;
+        }
+        if(flowerbed[flowerbed.Length-1] == 0 && flowerbed[flowerbed.Length-2]== 0){
+            x=x+1;
+        }
+        
+        for(int i=2; i<flowerbed.Length-2;i++){
+           if(flowerbed[i]==0){
+                j=j+1;
+            }else{
+                j=0;
+            }
+            
+            if(j==2){
+                x=x+1;
+                j = 0;
+            }
+        }
+        int n= 2;
+        Console.WriteLine(x);
+        if(n <= x){
+            Console.WriteLine(true);
+        }else{
+            Console.WriteLine(false);
+        }
+
+    
+        
